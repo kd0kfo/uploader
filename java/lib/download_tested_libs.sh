b96995ca713dff48179afd2e0befb37773eee3fc
David Coss <david@davecoss.com>
2014-01-11 15:28:07 -0600

    Added json simple jar to download list.

--- java/lib/download_tested_libs.sh
+++ java/lib/download_tested_libs.sh
@@ -6,4 +6,4 @@ wget http://repo1.maven.org/maven2/commons-codec/commons-codec/1.9/commons-codec
 wget http://repo1.maven.org/maven2/org/apache/httpcomponents/httpclient/4.3.1/httpclient-4.3.1.jar
 wget http://repo1.maven.org/maven2/org/apache/httpcomponents/httpcore/4.3.1/httpcore-4.3.1.jar
 wget http://repo1.maven.org/maven2/org/apache/httpcomponents/httpmime/4.3.1/httpmime-4.3.1.jar
-
+wget https://json-simple.googlecode.com/files/json-simple-1.1.1.jar
