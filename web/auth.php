f33eb02e8741e9aeeda77fababf117335f18134a
David Coss <david@davecoss.com>
2014-02-27 11:00:37 -0600

    Refactored auth.php

--- web/auth.php
+++ web/auth.php
@@ -5,20 +5,25 @@ require_once("site_db.inc");
 
 class Auth {
 
+	var $username;
+	
+	function Auth($user) {
+		$this->username = $user;
+	}
+	
 	function verify_logon() {
-		$username = get_requested_string("username");
 		$hmac = get_requested_string("hmac");
 
-		$retval = $this->verify_user($username, $hmac);	
+		$retval = $this->verify_user($hmac);	
 		return $retval;
 	}
 
-	function verify_user($username, $hmac) {
-		if($this->excessive_failed_logins($username)) {
+	function verify_user($hmac) {
+		if($this->excessive_failed_logins($this->username)) {
 			json_exit("Maximum failed logins.", 1);
 		}
 
-		$result = sql_query("select passhash from users where username = '$username';");
+		$result = sql_query("select passhash from users where username = '$this->username';");
 		if(!$result) {
 			return false;
 		}
@@ -29,7 +34,7 @@ class Auth {
 
 		$retval = ($row['passhash'] == $hmac);
 		if(!$retval) {
-			$this->increment_failed_logins($username);
+			$this->increment_failed_logins($this->username);
 		}
 		
 		return $retval;
@@ -39,29 +44,29 @@ class Auth {
 	 * Verifies that the user signed the data with the password.
 	 * For session signed data, see authenticate.
 	 */
-	function verify_data($username, $data, $hash) {
-		if(!$username) {
+	function verify_data($data, $hash) {
+		if(!$this->username) {
 			json_exit("Missing username", 1);
 		}
 
-		return $hash == $this->get_user_hash($username, $data);
+		return $hash == $this->get_user_hash($data);
 	}
 
-	function create_session_key($username) {
+	function create_session_key() {
 		global $site_secret;
 		$data = strval(time());
 
 		$now = time();
 		$data = strval($now);
-		$sessionkey = $this->hash($data, $site_secret);
+		$sessionkey = auth_hash($data, $site_secret);
 		
 
-		sql_exec("update users set sessionkey = '" . $sessionkey . "', sessionstart = ". $now . ";");
-		return $this->get_session_key($username);
+		sql_exec("update users set sessionkey = '" . $sessionkey . "', sessionstart = ". $now . " where username = '" . $this->username . "';");
+		return $this->get_session_key();
 	}
 
-	function get_session_key($username) {
-		$result = sql_query("select sessionkey, sessionstart, failed_logins from users where username = '$username';");
+	function get_session_key() {
+		$result = sql_query("select sessionkey, sessionstart, failed_logins from users where username = '" . $this->username . "';");
 		if(!$result) {
 			return null;
 		}
@@ -69,7 +74,7 @@ class Auth {
 		if(!$row) {
 			return null;
 		}
-		if($this->excessive_failed_logins($username)) {
+		if($this->excessive_failed_logins()) {
 			json_exit("Max failed logins", 1);
 		}
 		if(!$row['sessionkey'] || !$row['sessionstart'] || $row['sessionstart'] < 86400) {
@@ -78,63 +83,64 @@ class Auth {
 		return $row['sessionkey'];
 	}
 
-	function clear_session_key($username) {
-		sql_exec("update users set sessionkey = null, sessionstart = null where username = '$username';");
+	function clear_session_key() {
+		sql_exec("update users set sessionkey = null, sessionstart = null where username = '" . $this->username . "';");
 	}
 
-	function increment_failed_logins($username) {
-		sql_exec("update users set failed_logins = failed_logins + 1 where username = '$username';");
+	function increment_failed_logins() {
+		sql_exec("update users set failed_logins = failed_logins + 1 where username = '" . $this->username . "';");
 	}
 
-	function get_failed_logins($username) {
-		$result = sql_query("select failed_logins from users where username='$username';") or die("Failed database access");
+	function get_failed_logins() {
+		$result = sql_query("select failed_logins from users where username='" . $this->username . "';") or die("Failed database access");
 		$row = $result->fetchArray();
 		if(!$row) {
-			die("Unknown user $username");
+			die("Unknown user " . $this->username);
 		}
 		return $row['failed_logins'];
 	}
 		
-	function excessive_failed_logins($username) {
+	function excessive_failed_logins() {
 		global $max_failed_logins;
-		$result = $this->get_failed_logins($username);
+		$result = $this->get_failed_logins();
 		return $result >= $max_failed_logins;
 	}
 
 	function authenticate($data, $signature) {
-		$username = get_requested_string("username");
-		if($this->excessive_failed_logins($username)) {
+		if($this->excessive_failed_logins()) {
 			json_exit("Maximum failed logins.", 1);
 		}
 
-		$sessionkey = $this->get_session_key($username);
+		$sessionkey = $this->get_session_key();
 		if(!$sessionkey) {
 			return false;
 		}
-		$realhash = $this->hash($data, $sessionkey);
+		$realhash = auth_hash($data, $sessionkey);
 		$retval = ($signature == $realhash);
 		if(!$retval) {
-			$this->increment_failed_logins($username);
+			$this->increment_failed_logins();
 		}
 		return $retval;
 	}
 
-	function get_user_hash($username, $data) {
-		$result = sql_query("select passhash from users where username = '$username'");
+	function get_user_hash($data) {
+		$result = sql_query("select passhash from users where username = '" . $this->username . "';");
 		$row = $result->fetchArray();
 		if($row == null || !$row['passhash']) {
 			return false;
 		}
 		$pass = $row['passhash'];
 
-		return $this->hash($data, $pass);
+		return auth_hash($data, $pass);
 	}
 
-	function hash($data, $secret) {
-		$h = hash_hmac('sha256', $data, $secret, true);
-		$h = base64_encode($h);
-		return $h;
-	}
+	
 
+}
+
+function auth_hash($data, $secret) {
+	$h = hash_hmac('sha256', $data, $secret, true);
+	$h = base64_encode($h);
+	return $h;
 }
 ?>
--- web/tests/authtest.php
+++ web/tests/authtest.php
@@ -7,30 +7,37 @@ class authtest extends PHPUnit_Framework_TestCase {
 
 	public function testOne() {
 		global $db;
-		$auth = new Auth();
 		$username = "authtest";
+		$auth = new Auth($username);
 		$db->exec("insert into users values ('$username', 'baWMoR/ZMBC/JI27QJJ+sw0hf85chWk6Ryhn3n5gaEc=', null, null, 0);");
 
 		/* Test verify */
-		$result = $auth->verify_user($username, "baWMoR/ZMBC/JI27QJJ+sw0hf85chWk6Ryhn3n5gaEc=");
+		$result = $auth->verify_user("baWMoR/ZMBC/JI27QJJ+sw0hf85chWk6Ryhn3n5gaEc=");
+		echo "Testing logon... ";
 		$this->assertTrue($result);
+		echo "Passed\n";
 		
 		/* Test data signing */
-		$sessionkey = $auth->create_session_key($username);
+		$sessionkey = $auth->create_session_key();
 
-		echo "Session hash: $sessionkey";
+		echo "Session hash: $sessionkey\n";
 
 		$data = "foo";
-		$hashval = $auth->hash($data, $sessionkey);
+		$hashval = auth_hash($data, $sessionkey);
 
-		$this->assertTrue($auth->authenticate($username, $data, $hashval));
+		echo "Testing authentication of message... ";
+		$this->assertTrue($auth->authenticate($data, $hashval));
+		echo "Passed.\n";
 
 		/* Test log off */
-		$auth->clear_session_key($username);
-		$this->assertFalse($auth->authenticate($username, $data, $hashval));
+		$auth->clear_session_key();
+		echo "Testing Logout... ";
+		$this->assertFalse($auth->authenticate($data, $hashval));
+		echo "Passed\n";
 
 
 		$db->exec("delete from users where username = 'authtest';");
+		echo "Cleaned up database\n";
 	}
 }
 
