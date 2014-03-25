5f6dda1bbb1bb9ec6bf78727a40b6ae40cfa7b21
David Coss <david@davecoss.com>
2014-03-25 15:27:04 -0500

    Added Download Reader.

--- java/src/com/davecoss/uploader/utils/CommonsBase64.java
+++ java/src/com/davecoss/uploader/utils/CommonsBase64.java
@@ -1,10 +1,12 @@
 package com.davecoss.uploader.utils;
 
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import com.davecoss.java.GenericBase64;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Base64InputStream;
 import org.apache.commons.codec.binary.Base64OutputStream;
 
 public class CommonsBase64 implements GenericBase64 {
@@ -27,4 +29,9 @@ public class CommonsBase64 implements GenericBase64 {
 		return new Base64OutputStream(baseStream);
 	}
 
+	@Override
+	public InputStream decodeInputStream(InputStream baseStream) {
+		return new Base64InputStream(baseStream);
+	}
+	
 }
