ee84d4cff9aaa612948a999b72dbc127264c7ac1
David Coss <david@davecoss.com>
2014-04-09 07:13:28 -0500

    Updated scripts.
    Run the export class path script. Source "classpath" and then scripts will know where to find jars

--- java/console/checkout.sh
+++ java/console/checkout.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-if [[ -z $UPLOADER_HOME ]];then
-	UPLOADER_HOME=$PWD
-fi
-
-CLASSPATH="$UPLOADER_HOME/build/jar/webfsdesktop-1.0.a.jar"
-
-for lib in $UPLOADER_HOME/lib/*.jar;do
-	CLASSPATH=$lib:$CLASSPATH
-done
-CLASSPATH=${CLASSPATH}
-
-java -cp $CLASSPATH com.davecoss.uploader.utils.CheckoutGUI $@
+java com.davecoss.uploader.utils.CheckoutGUI $@
