0bf04afbc36d356677ba72f41697d66a976ba029
David Coss <david@davecoss.com>
2014-06-12 10:13:35 -0500

    Removed webfscore repo.

--- java/console/maven_export_classpath.sh
+++ java/console/maven_export_classpath.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo export CLASSPATH=$PWD/target/webfsconsole-1.0.a.jar:$(mvn -Djsse.enableSNIExtension=false -Dwebfscore.repo.path=/path/to/localrepo dependency:build-classpath|grep -v "^\[") > classpath
+echo export CLASSPATH=$PWD/target/webfsconsole-1.0.a.jar:$(mvn -Djsse.enableSNIExtension=false dependency:build-classpath|grep -v "^\[") > classpath
 
 echo Wrote the following to $PWD/classpath
 cat classpath
