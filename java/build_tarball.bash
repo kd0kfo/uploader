934cc36d76487323f24ec7a507bba284f371ee80
David Coss <david@davecoss.com>
2014-06-14 11:15:08 -0500

    Added template to mktemp for mac

--- java/build_tarball.bash
+++ java/build_tarball.bash
@@ -11,7 +11,7 @@ mvn package
 CLASSPATH=$PWD/target/webfsconsole-1.0.a.jar:$(mvn -Djsse.enableSNIExtension=false -Dwebfscore.repo.path=$PWD/../corerepo dependency:build-classpath|grep -v "^\[")
 
 cd ..
-TMPDIR=$(mktemp -d)
+TMPDIR=$(mktemp -d webfsbuild.XXXX)
 ORIGDIR=$PWD
 cd $TMPDIR
 cp $(echo $CLASSPATH|tr : " ") .
